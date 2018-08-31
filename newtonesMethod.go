3a6e16fbb2a8d8ab42b7573caee536981bad1297
강성훈 <kangpual@naver.com>
2018-08-31 14:16:51 +0900

    들여쓰기 변경

--- newtonesMethod.go
+++ newtonesMethod.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 var i = 0
 var prev float64 = 0
 
 func Sqrt(x float64) float64 {
-	z := float64(1)
-	return rec(x, z)
+    z := float64(1)
+    return rec(x, z)
 }
 
 func rec(x float64, z float64) float64 {
     prev = z
-	y := z - (z*z-x)/(2*z)
+    y := z - (z*z-x)/(2*z)
     i++
-	if prev == 1 || prev > y {
-		return rec(x, y)
-	}
-	return y
+    if prev == 1 || prev > y {
+        return rec(x, y)
+    }
+    return y
 }
 
 func main() {
-	fmt.Println("loop count = ", i, "result = ", Sqrt(16))
+    fmt.Println("loop count = ", i, "result = ", Sqrt(16))
 }
